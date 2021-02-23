dbdcb8db7f7d517e39f80f563d859aad3f159948
Eunsoogil <59255463+Eunsoogil@users.noreply.github.com>
2021-02-23 13:42:24 +0900

    python study

--- webScraping/webscraping.py
+++ webScraping/webscraping.py
@@ -4,7 +4,59 @@
 # request library 필요 pip3 install requests
 import requests
 from bs4 import BeautifulSoup
+import pprint  # pretty print
 
 res = requests.get('https://news.ycombinator.com/news')  # html 받아옴
-print(res)
-print(res.text)
+# print(res)
+# print(res.text)
+
+soup = BeautifulSoup(res.text, 'html.parser')
+# print(soup)
+# print(soup.body.contents)
+# print(soup.find_all('div'))
+# print(soup.find(id='score_26222530'))
+# print(soup.select('.score'))  # css 선택자
+# print(soup.select('.storylink')[0])
+links = soup.select('.storylink')
+subtext = soup.select('.subtext')
+
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
+
+def create_custom_hn(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = votes[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
+
+
+# print(create_custom_hn(links, subtext))
+# pprint.pprint(create_custom_hn(links, subtext))
+
+
+def hackerNewsCrawling():
+    cnt = 0
+    li = []
+    sub = []
+    while True:
+        url = 'https://news.ycombinator.com/news?p=' + str(cnt)
+        req = requests.get(url)  # html 받아옴
+        result = BeautifulSoup(req.text, 'html.parser')
+        if not result.select('.storylink'):
+            break
+        li.extend(result.select('.storylink'))
+        sub.extend(result.select('.subtext'))
+        cnt += 1
+
+    pprint.pprint(create_custom_hn(li, sub))
+
+
+hackerNewsCrawling()
