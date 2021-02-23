bc7727a85ce834e94cdb068b06e7ee427b7c1935
Eunsoogil <59255463+Eunsoogil@users.noreply.github.com>
2021-02-24 08:56:16 +0900

    python study

---
+++ webServer/script.js
@@ -0,0 +1,4 @@
+// venv\Scripts\activate.bat
+// pip --version으로 virtual environment 확인
+// pip install Flask
+// d:\pythonstudy\webserver\venv\scripts\python.exe -m pip install --upgrade pip pip 업그레이드 하라고 나옴
