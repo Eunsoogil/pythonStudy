cb29da005ffb4e043c10073fd94a26ced9ea1c41
Eunsoogil <59255463+Eunsoogil@users.noreply.github.com>
2021-02-25 08:57:00 +0900

    python study

--- webServer/server.py
+++ webServer/server.py
@@ -1,5 +1,10 @@
-from flask import Flask
-from flask import render_template
+# python -m venv location
+# location\Scripts\activate.bat  >> 실행하면 콘솔 () 부분이 location으로 바뀜
+# pip --version으로 virtual environment 확인
+# pip install Flask
+# d:\pythonstudy\webserver\venv\scripts\python.exe -m pip install --upgrade pip pip 업그레이드 하라고 나옴
+
+from flask import Flask, render_template, url_for
 # cmd : C:\path\to\app>set FLASK_APP=hello.py
 # cmd : python -m flask run 또는 flask run
 # cmd : set FLASK_ENV=development 시 디버그 모드 on, 수정할때마다 바꿀 필요 없음
@@ -11,3 +16,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/<username>/<int:post_id>')
+def usernameRender(username=None, post_id=None):
+    # post_id의 경우 int형이 아니면 안됨(404)
+    return render_template('index.html', name=username, post_id=post_id)
