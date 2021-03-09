c0ac9faad1ab6aeeab0f0131e77676f59a107253
Eunsoogil <59255463+Eunsoogil@users.noreply.github.com>
2021-03-09 09:03:16 +0900

    Create main.py

---
+++ machineLearning/main.py
@@ -0,0 +1,16 @@
+# 1 - import the data
+# 2 - clean the data
+# 3 - split data. training set(input)/test set(output)
+# 4 - create a model
+# 5 - check the output
+# 6 - improve
+
+# 데이터를 모으기가 가장 어렵다
+
+# tools
+# numpy : list 등의 정보 가공용
+# pandas : csv 등의 정보 가공용
+# scikit-learn : model 생성
+# matplotlib : visualize data
+# jupyter notebook : 과정 정리용
+# kaggle : 머신러닝 경쟁 사이트, 무료 data 셋이 있음
